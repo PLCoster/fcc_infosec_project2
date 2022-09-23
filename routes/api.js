1dffafd0291a399ad772e565f3b7423b2473cb9a
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-09-23 13:11:49 -0700

    Basic implementation of GET and POST /api/threads/:board routes in routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,24 @@
 'use strict';
 
-module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+const {
+  createThread,
+  getTenMostRecentThreads,
+} = require('../controllers/threadcontroller');
 
+module.exports = function (app) {
+  app
+    .route('/api/threads/:board')
+
+    // GET request to /api/threads/board returns 10 most recent Threads for board
+    // - each Thread has the 3 most recent replies
+    .get(getTenMostRecentThreads, (req, res) => {
+      return res.json(res.locals.boardThreads);
+    })
+
+    // POST request to /api/threads/board creates a new Thread on given board
+    .post(createThread, (req, res) => {
+      return res.json(res.locals.createdThread);
+    });
+
+  app.route('/api/replies/:board');
 };
